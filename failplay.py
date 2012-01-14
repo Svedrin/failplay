9769af29e3f00ceb634b7545c1d7c23d60669417
Michael Ziegler <diese-addy@funzt-halt.net>
2012-01-14 22:31:49 +0100

    implement en/dequeue, repeat and stopAfter in the GUI

--- failplay.py
+++ failplay.py
@@ -34,6 +34,22 @@ class FailPlay(Ui_MainWindow, QtGui.QMainWindow ):
         self.connect( self.player,           Player.sig_position_normal,     self.status_update_normal )
         self.connect( self.player,           Player.sig_position_trans,      self.status_update_trans  )
 
+        self.actEnqueue = QtGui.QAction("Enqueue", self.lstPlaylist)
+        self.connect( self.actEnqueue, QtCore.SIGNAL("triggered(bool)"), self.enqueue)
+        self.lstPlaylist.insertAction(None, self.actEnqueue)
+
+        self.actDequeue = QtGui.QAction("Dequeue", self.lstPlaylist)
+        self.connect( self.actDequeue, QtCore.SIGNAL("triggered(bool)"), self.dequeue)
+        self.lstPlaylist.insertAction(None, self.actDequeue)
+
+        self.actRepeat = QtGui.QAction("Repeat", self.lstPlaylist)
+        self.connect( self.actRepeat, QtCore.SIGNAL("triggered(bool)"), self.repeat)
+        self.lstPlaylist.insertAction(None, self.actRepeat)
+
+        self.actStopAfter = QtGui.QAction("Stop after this track", self.lstPlaylist)
+        self.connect( self.actStopAfter, QtCore.SIGNAL("triggered(bool)"), self.stopafter)
+        self.lstPlaylist.insertAction(None, self.actStopAfter)
+
         self.show()
 
     def play_pause(self, checked):
@@ -50,6 +66,32 @@ class FailPlay(Ui_MainWindow, QtGui.QMainWindow ):
 
     def start(self):
         self.player.start()
+
+    def enqueue(self):
+        path = self.lstPlaylist.currentItem().data(Qt.Qt.UserRole).toString().toLocal8Bit().data()
+        self.playlist.enqueue(path)
+
+    def dequeue(self):
+        path = self.lstPlaylist.currentItem().data(Qt.Qt.UserRole).toString().toLocal8Bit().data()
+        self.playlist.dequeue(path)
+
+    def repeat(self):
+        path = self.lstPlaylist.currentItem().data(Qt.Qt.UserRole).toString().toLocal8Bit().data()
+        idx = self.playlist.playlist.index(path)
+        if self.playlist.repeat == idx:
+            self.playlist.repeat = None
+        else:
+            self.playlist.repeat = idx
+        self.update_playlist()
+
+    def stopafter(self):
+        path = self.lstPlaylist.currentItem().data(Qt.Qt.UserRole).toString().toLocal8Bit().data()
+        idx = self.playlist.playlist.index(path)
+        if self.playlist.stopafter == idx:
+            self.playlist.stopafter = None
+        else:
+            self.playlist.stopafter = idx
+        self.update_playlist()
 
     def closeEvent(self, ev):
         self.player.stop()
@@ -70,11 +112,24 @@ class FailPlay(Ui_MainWindow, QtGui.QMainWindow ):
         print "Redraw.", self.playlist.current
         for idx, path in enumerate(self.playlist.playlist):
             item = QtGui.QListWidgetItem()
-            item.setData(Qt.Qt.DisplayRole, path.rsplit( '/', 1 )[1].rsplit('.', 1)[0])
+
+            display = path.rsplit( '/', 1 )[1].rsplit('.', 1)[0]
+            modifiers = []
+            if path in self.playlist.jmpqueue:
+                modifiers.append( unicode(self.playlist.jmpqueue.index(path) + 1) )
+            if idx == self.playlist.repeat:
+                modifiers.append( u'♻' )
+            if idx == self.playlist.stopafter:
+                modifiers.append( u'▇' )
+            if modifiers:
+                display += " [%s]" % ''.join(modifiers)
+            item.setData(Qt.Qt.DisplayRole, display)
+
             item.setData(Qt.Qt.UserRole, path)
+
             if idx == self.playlist.current:
-                print "selected", path
                 item.setBackground( QtGui.QBrush(Qt.Qt.cyan, Qt.Qt.SolidPattern) )
+
             self.lstPlaylist.addItem(item)
 
 
