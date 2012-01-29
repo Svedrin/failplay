7a06153bf5be14e6519f412bb50c16bc17a1b966
Michael Ziegler <diese-addy@funzt-halt.net>
2012-01-29 14:01:28 +0100

    add player example

---
+++ myffmpeg/player_example.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# kate: space-indent on; indent-width 4; replace-tabs on;
+
+# The most simple audio player in existance.
+
+import sys
+import ffmpeg
+import ao
+
+pcm = ao.AudioDevice("pulse")
+
+rdr = ffmpeg.Decoder(sys.argv[-1])
+
+for chunk in rdr.read():
+    pcm.play( chunk )
