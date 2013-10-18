add475695df267ea0cfc4fe33a986fd6ced29147
Michael Ziegler <diese-addy@funzt-halt.net>
2013-10-18 23:52:59 +0200

    add analyzers to gui, change layout

--- ui_failplay.py
+++ ui_failplay.py
@@ -2,8 +2,8 @@
 
 # Form implementation generated from reading ui file 'failplay.ui'
 #
-# Created: Fri Jan 20 20:38:10 2012
-#      by: PyQt4 UI code generator 4.9
+# Created: Fri Oct 18 22:11:29 2013
+#      by: PyQt4 UI code generator 4.10.3
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -12,16 +12,25 @@ from PyQt4 import QtCore, QtGui
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
 except AttributeError:
-    _fromUtf8 = lambda s: s
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(632, 469)
+        MainWindow.resize(632, 662)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
-        self.verticalLayout_3 = QtGui.QVBoxLayout(self.centralwidget)
-        self.verticalLayout_3.setObjectName(_fromUtf8("verticalLayout_3"))
+        self.verticalLayout_4 = QtGui.QVBoxLayout(self.centralwidget)
+        self.verticalLayout_4.setObjectName(_fromUtf8("verticalLayout_4"))
         self.splitter = QtGui.QSplitter(self.centralwidget)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
         sizePolicy.setHorizontalStretch(0)
@@ -62,26 +71,23 @@ class Ui_MainWindow(object):
         self.lstPlaylist.setSelectionMode(QtGui.QAbstractItemView.SingleSelection)
         self.lstPlaylist.setSelectionBehavior(QtGui.QAbstractItemView.SelectRows)
         self.lstPlaylist.setObjectName(_fromUtf8("lstPlaylist"))
-        self.verticalLayout_3.addWidget(self.splitter)
-        self.horizontalLayout_2 = QtGui.QHBoxLayout()
-        self.horizontalLayout_2.setSizeConstraint(QtGui.QLayout.SetMinimumSize)
-        self.horizontalLayout_2.setObjectName(_fromUtf8("horizontalLayout_2"))
+        self.verticalLayout_4.addWidget(self.splitter)
+        self.horizontalLayout = QtGui.QHBoxLayout()
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
         self.verticalLayout = QtGui.QVBoxLayout()
         self.verticalLayout.setSizeConstraint(QtGui.QLayout.SetMinimumSize)
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.anzSong = QFftAnalyzer(self.centralwidget)
+        self.anzSong.setMinimumSize(QtCore.QSize(0, 100))
+        self.anzSong.setObjectName(_fromUtf8("anzSong"))
+        self.verticalLayout.addWidget(self.anzSong)
         self.pgbSongProgress = QtGui.QProgressBar(self.centralwidget)
         self.pgbSongProgress.setProperty("value", 0)
         self.pgbSongProgress.setTextVisible(True)
         self.pgbSongProgress.setInvertedAppearance(False)
         self.pgbSongProgress.setObjectName(_fromUtf8("pgbSongProgress"))
         self.verticalLayout.addWidget(self.pgbSongProgress)
-        self.pgbSongProgressPrev = QtGui.QProgressBar(self.centralwidget)
-        self.pgbSongProgressPrev.setProperty("value", 0)
-        self.pgbSongProgressPrev.setTextVisible(True)
-        self.pgbSongProgressPrev.setInvertedAppearance(False)
-        self.pgbSongProgressPrev.setObjectName(_fromUtf8("pgbSongProgressPrev"))
-        self.verticalLayout.addWidget(self.pgbSongProgressPrev)
-        self.horizontalLayout_2.addLayout(self.verticalLayout)
+        self.horizontalLayout.addLayout(self.verticalLayout)
         self.sldCrossfade = QtGui.QSlider(self.centralwidget)
         self.sldCrossfade.setEnabled(False)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
@@ -92,13 +98,26 @@ class Ui_MainWindow(object):
         self.sldCrossfade.setMaximum(100)
         self.sldCrossfade.setProperty("value", 100)
         self.sldCrossfade.setSliderPosition(100)
-        self.sldCrossfade.setOrientation(QtCore.Qt.Vertical)
+        self.sldCrossfade.setOrientation(QtCore.Qt.Horizontal)
         self.sldCrossfade.setObjectName(_fromUtf8("sldCrossfade"))
-        self.horizontalLayout_2.addWidget(self.sldCrossfade)
-        self.verticalLayout_3.addLayout(self.horizontalLayout_2)
+        self.horizontalLayout.addWidget(self.sldCrossfade)
+        self.verticalLayout_3 = QtGui.QVBoxLayout()
+        self.verticalLayout_3.setObjectName(_fromUtf8("verticalLayout_3"))
+        self.anzPrev = QFftAnalyzer(self.centralwidget)
+        self.anzPrev.setMinimumSize(QtCore.QSize(0, 100))
+        self.anzPrev.setObjectName(_fromUtf8("anzPrev"))
+        self.verticalLayout_3.addWidget(self.anzPrev)
+        self.pgbSongProgressPrev = QtGui.QProgressBar(self.centralwidget)
+        self.pgbSongProgressPrev.setProperty("value", 0)
+        self.pgbSongProgressPrev.setTextVisible(True)
+        self.pgbSongProgressPrev.setInvertedAppearance(False)
+        self.pgbSongProgressPrev.setObjectName(_fromUtf8("pgbSongProgressPrev"))
+        self.verticalLayout_3.addWidget(self.pgbSongProgressPrev)
+        self.horizontalLayout.addLayout(self.verticalLayout_3)
+        self.verticalLayout_4.addLayout(self.horizontalLayout)
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(MainWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 632, 22))
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 632, 20))
         self.menubar.setObjectName(_fromUtf8("menubar"))
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtGui.QStatusBar(MainWindow)
@@ -109,9 +128,10 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
     def retranslateUi(self, MainWindow):
-        MainWindow.setWindowTitle(QtGui.QApplication.translate("MainWindow", "FailPlay", None, QtGui.QApplication.UnicodeUTF8))
-        self.leLibraryFilter.setPlaceholderText(QtGui.QApplication.translate("MainWindow", "Search...", None, QtGui.QApplication.UnicodeUTF8))
-        self.pgbSongProgress.setFormat(QtGui.QApplication.translate("MainWindow", "Idle", None, QtGui.QApplication.UnicodeUTF8))
-        self.pgbSongProgressPrev.setFormat(QtGui.QApplication.translate("MainWindow", "Idle", None, QtGui.QApplication.UnicodeUTF8))
+        MainWindow.setWindowTitle(_translate("MainWindow", "FailPlay", None))
+        self.leLibraryFilter.setPlaceholderText(_translate("MainWindow", "Search...", None))
+        self.pgbSongProgress.setFormat(_translate("MainWindow", "Idle", None))
+        self.pgbSongProgressPrev.setFormat(_translate("MainWindow", "Idle", None))
 
 from libraryview import LibraryView
+from analyzer import QFftAnalyzer
